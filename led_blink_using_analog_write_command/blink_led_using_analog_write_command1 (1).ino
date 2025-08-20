60c59226ad339e4cebd0bd2b2e17610752e09085
Mounikagit config user.name git config --global user.name Mounika <mouni141188@gmail.com>
2025-08-20 18:47:41 +0530

    add comments

--- led_blink_using_analog_write_command/blink_led_using_analog_write_command1 (1).ino
+++ led_blink_using_analog_write_command/blink_led_using_analog_write_command1 (1).ino
@@ -1,5 +1,5 @@
-int redPin=9;
-int bright=250;
+int redPin=9;//by using redwire pin 9 connected to the resister.
+int bright=250;//led intensity range.
 
 void setup()
 {
@@ -7,11 +7,13 @@ void setup()
 }
 
 void loop()
-{
+{	
+  //led intensity will be increased between 0-250.
   for(int i=0;i<bright;i++){
     analogWrite(redPin,i);
     delay(20);
   }
+  //led intensity will be decresed between 250-0.
   for(int j=bright;j>=0;j--){
     analogWrite(redPin,j);
     delay(20);
